a6fc236dfd752ad31b163c18bd35384d8971d308
Kiruthiga M <156992507+Kiruthiga1908@users.noreply.github.com>
2026-02-11 12:24:05 +0530

    Add files via upload

---
+++ DoubletoStringConversion.java
@@ -0,0 +1,19 @@
+package pst;
+
+import java.util.Scanner;
+
+public class DoubletoStringConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a double number: ");
+        double num = sc.nextDouble();
+
+        
+        String str = Double.toString(num);
+
+        System.out.println("Converted String is: " + str);
+
+        sc.close();
+    }
+}
