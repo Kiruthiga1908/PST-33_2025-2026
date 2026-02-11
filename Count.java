8f7d48b9fe2e4fc84d13597a0ddd7612379f0e01
Kiruthiga M <156992507+Kiruthiga1908@users.noreply.github.com>
2026-02-11 12:22:50 +0530

    Add files via upload

--- Count.java
+++ Count.java
@@ -1,10 +1,29 @@
 package PST_JAVA;
 
+
+
+import java.util.Scanner;
+
 public class Count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
 
-	}
+        int count = 0;
+        str = str.toLowerCase(); // convert to lowercase for easy comparison
 
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || 
+                ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println("Number of vowels: " + count);
+        sc.close();
+    }
 }
