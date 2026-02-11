02f342eeccc182d9a601dd9c034ecbdb18b50f71
Kiruthiga M <156992507+Kiruthiga1908@users.noreply.github.com>
2026-02-11 12:13:20 +0530

    Add files via upload

---
+++ AddTwo.java
@@ -0,0 +1,11 @@
+package pst;
+
+import java.util.Scanner;
+class AddTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum: " + (a + b));
+    }
+}
